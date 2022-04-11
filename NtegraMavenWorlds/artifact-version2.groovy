dc0db3934679f1a3acfee19134b0caa1795fe550
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-11 16:44:42 +0400

    Test

--- NtegraMavenWorlds/artifact-version2.groovy
+++ NtegraMavenWorlds/artifact-version2.groovy
@@ -1,7 +1,7 @@
 import hudson.model.*;
 import hudson.util.*;
-def thr = Thread.currentThread();
-def currentBuild = thr?.executable;
+
+def build = Thread.currentThread().executable
 def mavenVer = currentBuild.getParent().getModules().toArray()[0].getVersion();
 def newParamAction = new hudson.model.ParametersAction(new hudson.model.StringParameterValue("ARTIFACT_VERSION", mavenVer));
 currentBuild.addAction(newParamAction);
