3a6683b9db5015bcb24f80570a9a71a925d6f7d8
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-12 16:21:15 +0400

    -Removing Jenkins Build Number from Version
    -Update release creation with generated artifact file name

--- NtegraMavenWorlds/create_release.sh
+++ NtegraMavenWorlds/create_release.sh
@@ -1,11 +1,13 @@
 export
 # Publish on github
+cd $POM_ARTIFACTID/target
 echo "Publishing on Github..."
 token=$NTGERAGITPUBLISHER
 # Get the last tag name
 tag=$(git describe --tags)
 # Get the full message associated with this tag
 message="$(git for-each-ref refs/tags/$tag --format='%(contents)')"
+artifactfile=$POM_ARTIFACTID_$POM_VERSION_$BUILD_NUMBER.$POM_PACKAGING
 # Get the title and the description as separated variables
 name=$(echo "$message" | head -n1)
 description=$(echo "$message" | tail -n +3)
@@ -15,4 +17,4 @@ release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 # Upload the artifact
-curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=artifact.zip
+curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @$artifactfile https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=$artifactfile
