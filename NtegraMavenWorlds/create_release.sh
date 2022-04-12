116622ea04b733dad1c827b91595dd582b83bdcf
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-12 16:53:29 +0400

    Add debugging

--- NtegraMavenWorlds/create_release.sh
+++ NtegraMavenWorlds/create_release.sh
@@ -18,8 +18,11 @@ name=$(echo "$message" | head -n1)
 description=$(echo "$message" | tail -n +3)
 description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
 # Create a release
+echo "Create a release"
 release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"main\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/ntegra-cmp/ntegra-maven-world/releases)
+echo "Release creation result : $release"
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+echo "Extract Release ID: $id" 
 # Upload the artifact
 curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @$artifactfile https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=$artifactfile
