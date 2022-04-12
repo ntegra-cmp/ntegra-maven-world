bfae8317b3e62fc53ec88003a0c01d0ca5b8fa92
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-12 12:06:21 +0400

    Committing the create release on github shell script to be executed after successful builds only

---
+++ NtegraMavenWorlds/create_release.sh
@@ -0,0 +1,18 @@
+export
+# Publish on github
+echo "Publishing on Github..."
+token="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"
+# Get the last tag name
+tag=$(git describe --tags)
+# Get the full message associated with this tag
+message="$(git for-each-ref refs/tags/$tag --format='%(contents)')"
+# Get the title and the description as separated variables
+name=$(echo "$message" | head -n1)
+description=$(echo "$message" | tail -n +3)
+description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
+# Create a release
+release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"main\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/ntegra-cmp/ntegra-maven-world/releases)
+# Extract the id of the release from the creation response
+id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
+# Upload the artifact
+curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=artifact.zip
