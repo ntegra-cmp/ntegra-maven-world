bc2bd0c0d5ce059b745d3bb45e817a1ef1f87d3e
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-20 15:04:04 +0400

    fix syntax typo

--- NtegraMavenWorlds/create_version.sh
+++ NtegraMavenWorlds/create_version.sh
@@ -1,7 +1,7 @@
 export
 NTG_DEPLOYTYPE=fetch
-NTG_VERSION=$(cat ntg_build.properties|grep NTG_RELEASE_NAME|cut -d '=' -f 2))
-NTG_FETCHRUL=$(cat ntg_build.properties|grep RELEASE_DOWNLOAD_URL|cut -d '=' -f 2))
+NTG_VERSION=$(cat ntg_build.properties|grep NTG_RELEASE_NAME|cut -d '=' -f 2)
+NTG_FETCHRUL=$(cat ntg_build.properties|grep RELEASE_DOWNLOAD_URL|cut -d '=' -f 2)
 #NTG_ACCESSTOKEN=1500 # read from jenkins build parameters
 #NTG_DEPLOYMENTID=18  # read from jenkins build parameters
 curl -XPOST "https://ntegralab.sts-cloud.com/api/deployments/$NTG_DEPLOYMENTID/versions" -H "Authorization: Bearer $NTG_ACCESSTOKEN" -H "Content-Type: application/json" -d "{\"version\": {\"version\": \"$NTG_VERSION\",\"deployType\":\"$NTG_DEPLOYTYPE\",\"fetchUrl\": \"$NTG_FETCHRUL\"}}"
