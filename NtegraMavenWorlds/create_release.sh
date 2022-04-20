b7fea5eb7176c07564cf119d29025e09abc98f26
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-20 14:15:36 +0400

    exporting variables for later use while deploying the application.
    Extracting the download-able artifact URL

--- NtegraMavenWorlds/create_release.sh
+++ NtegraMavenWorlds/create_release.sh
@@ -9,12 +9,14 @@ tag=$(git describe --tags)
 # Get the full message associated with this tag
 message="$(git for-each-ref refs/tags/$tag --format='%(contents)')"
 artifactfile=$POM_ARTIFACTID"_"$POM_VERSION"_"$BUILD_NUMBER.$POM_PACKAGING
+export NTG_ARTIFACTFILENAME=$artifactfile
 echo "Artifcat Filename : $artifactfile"
 filelcation=`pwd`
 echo "Listing File Location: $filelcation"  
 ls -l 
 # Get the title and the description as separated variables
 name=$(echo "$message" | head -n1)
+export NTG_RELEASE_NAME=$name
 description=$(echo "$message" | tail -n +3)
 description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
 # Create a release
@@ -25,4 +27,5 @@ echo "Release creation result : $release"
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 echo "Extract Release ID: $id" 
 # Upload the artifact
-curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @$artifactfile https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=$artifactfile
+DOWNLOAD_URL=$(curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @$artifactfile https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=$artifactfile | grep browser_download_url  | cut -d '"' -f 4)
+echo "Download URL: $DOWNLOAD_URL"  
