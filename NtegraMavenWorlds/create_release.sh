12cba91344e04bb7ff5023cec47cb03258fafb36
Meanj <Meanj@PN-Maen.mshome.net>
2022-04-20 15:35:16 +0400

    General Fixes

--- NtegraMavenWorlds/create_release.sh
+++ NtegraMavenWorlds/create_release.sh
@@ -1,8 +1,10 @@
+logSuffix="create_release.sh:"
+echo $logSuffix
 export
 # Publish on github
-echo " Switching to packaging folder $POM_ARTIFACTID/target"
+echo "$logSuffix Switching to packaging folder $POM_ARTIFACTID/target"
 cd $POM_ARTIFACTID/target
-echo "Publishing on Github..."
+echo "$logSuffix Publishing on Github..."
 token=$NTGERAGITPUBLISHER_TOKEN
 # Get the last tag name
 tag=$(git describe --tags)
@@ -10,29 +12,27 @@ tag=$(git describe --tags)
 message="$(git for-each-ref refs/tags/$tag --format='%(contents)')"
 artifactfile=$POM_ARTIFACTID"_"$POM_VERSION"_"$BUILD_NUMBER.$POM_PACKAGING
 export NTG_ARTIFACTFILENAME=$artifactfile
-echo "Artifcat Filename : $artifactfile"
+echo "$logSuffix Artifcat Filename : $artifactfile"
 filelcation=`pwd`
-echo "Listing File Location: $filelcation"  
+echo "$logSuffix Listing File Location: $filelcation"  
 ls -l 
 # Get the title and the description as separated variables
 name=$(echo "$message" | head -n1)
 description=$(echo "$message" | tail -n +3)
 description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
 # Create a release
-echo "Create a release"
+echo "$logSuffix Create a release"
 release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"main\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/ntegra-cmp/ntegra-maven-world/releases)
-echo "Release creation result : $release"
+echo "$logSuffix Release creation result : $release"
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
-echo "Extract Release ID: $id" 
+echo "$logSuffix Extract Release ID: $id" 
 # Upload the artifact
 release_json=$(curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @$artifactfile https://uploads.github.com/repos/ntegra-cmp/ntegra-maven-world/releases/$id/assets?name=$artifactfile)
-echo $release_json
+echo "$logSuffix $release_json"
 RELEASE_DOWNLOAD_URL=$(echo $release_json | jq '.browser_download_url')
-echo "Download URL: $RELEASE_DOWNLOAD_URL"
+echo "$logSuffix Download URL: $RELEASE_DOWNLOAD_URL"
 
 # Saving Properties for next scripts
-echo "NTG_RELEASE_NAME=$name" > ntg_build.properties
-echo "RELEASE_DOWNLOAD_URL=$RELEASE_DOWNLOAD_URL" >> ntg_build.properties
-pwd
-ls
+echo "$logSuffix NTG_RELEASE_NAME=$name" > ntg_build.properties
+echo "$logSuffix RELEASE_DOWNLOAD_URL=$RELEASE_DOWNLOAD_URL" >> ntg_build.properties
--- NtegraMavenWorlds/create_version.sh
+++ NtegraMavenWorlds/create_version.sh
@@ -1,7 +1,13 @@
+logSuffix="create_version.sh:"
+echo $logSuffix
+
 ARTIFACTPATH=$WORKSPACE/$POM_ARTIFACTID/target
 NTG_DEPLOYTYPE=fetch
 NTG_VERSION=$(cat $ARTIFACTPATH/ntg_build.properties|grep NTG_RELEASE_NAME|cut -d '=' -f 2)
 NTG_FETCHRUL=$(cat $ARTIFACTPATH/ntg_build.properties|grep RELEASE_DOWNLOAD_URL|cut -d '=' -f 2)
+echo "$logSuffix NTG_VERSION: $NTG_VERSION"
+echo "$logSuffix NTG_FETCHRUL: $NTG_FETCHRUL" 
 #NTG_ACCESSTOKEN=1500 # read from jenkins build parameters
 #NTG_DEPLOYMENTID=18  # read from jenkins build parameters
-curl -XPOST "https://ntegralab.sts-cloud.com/api/deployments/$NTG_DEPLOYMENTID/versions" -H "Authorization: Bearer $NTG_ACCESSTOKEN" -H "Content-Type: application/json" -d "{\"version\": {\"version\": \"$NTG_VERSION\",\"deployType\":\"$NTG_DEPLOYTYPE\",\"fetchUrl\": \"$NTG_FETCHRUL\"}}"
+jsonResult=$(curl -XPOST "https://ntegralab.sts-cloud.com/api/deployments/$NTG_DEPLOYMENTID/versions" -H "Authorization: Bearer $NTG_ACCESSTOKEN" -H "Content-Type: application/json" -d "{\"version\": {\"version\": \"$NTG_VERSION\",\"deployType\":\"$NTG_DEPLOYTYPE\",\"fetchUrl\": \"$NTG_FETCHRUL\"}}")
+echo "$logSuffix $jsonResult"
