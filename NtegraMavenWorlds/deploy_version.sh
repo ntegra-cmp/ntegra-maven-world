9f279b91221764890c214a7edd135d9587f8d133
Meanj <Meanj@PN-Maen.mshome.net>
2022-05-17 10:56:05 +0400

    Just Triggering a CI/CD

--- NtegraMavenWorlds/deploy_version.sh
+++ NtegraMavenWorlds/deploy_version.sh
@@ -2,7 +2,7 @@ logSuffix="deploy_version.sh:"
 echo $logSuffix 
 ARTIFACTPATH=$WORKSPACE/$POM_ARTIFACTID/target
 NTG_VERSION=$(cat $ARTIFACTPATH/ntg_build.properties|grep NTG_RELEASE_NAME|cut -d '=' -f 2)
-#NTG_ACCESSTOKEN=**** # read from jenkins build parameters
+#NTG_ACCESSTOKEN=**** # read from jenkins build parameters #
 #NTG_DEPLOYMENTID=18  # read from jenkins build parameters
 #NTG_INSTANCEID=1500  # read from jenkins build parameters
 #NTG_APISERVERURL=https://ntegralab.sts-cloud.com # read from jenkins build parameters
