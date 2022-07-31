b0354a4a73d2a60009acf4394d3001c2ccbacc75
Meanj <Meanj@PN-Maen.mshome.net>
2022-07-31 16:13:02 +0400

    Just Commit

--- NtegraMavenWorlds/deploy_version.sh
+++ NtegraMavenWorlds/deploy_version.sh
@@ -4,7 +4,7 @@ ARTIFACTPATH=$WORKSPACE/$POM_ARTIFACTID/target
 NTG_VERSION=$(cat $ARTIFACTPATH/ntg_build.properties|grep NTG_RELEASE_NAME|cut -d '=' -f 2)
 #NTG_ACCESSTOKEN=**** # read from jenkins build parameters #
 #NTG_DEPLOYMENTID=18  # read from jenkins build parameters #
-#NTG_INSTANCEID=1500  # read from jenkins build parameters #
+#NTG_INSTANCEID=1500  # read from jenkins build parameters  #
 #NTG_APISERVERURL=https://ntegralab.sts-cloud.com # read from jenkins build parameters
 jsonResult=$(curl -XPOST "$NTG_APISERVERURL/api/instances/$NTG_INSTANCEID/deploys" -H "Authorization: Bearer $NTG_ACCESSTOKEN" -H "Content-Type: application/json" -d "{\"appDeploy\": {\"deploymentId\": \"$NTG_DEPLOYMENTID\",\"version\": \"$NTG_VERSION\"}}")
 echo "$logSuffix $jsonResult"
